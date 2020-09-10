c9c70dce0ef7a4dc7e8828f4a0974e35cc709473
N. M. Ali Hayder <ahayder@loobylab.com>
2020-09-11 03:01:38 +0600

    my very first portfolio

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -82,7 +82,7 @@ const Contact = ({ data }) => {
 					   <h4>Address and Phone</h4>
 					   <p className="address">
 						   {data?.name}<br />
-						   {data?.address.street} <br />
+						   {/* {data?.address.street} <br /> */}
 						   {data?.address.city}, {data?.address.state} {data?.address.zip}<br />
 						   <span>{data?.phone}</span>
 					   </p>
